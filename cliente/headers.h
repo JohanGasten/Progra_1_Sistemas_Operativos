35c4f7b93a71081b543cd450bd863b8e89dce69f
Jeffrey <jeffny2015@gmail.com>
2018-10-13 16:51:53 -0600

    1111

--- cliente/headers.h
+++ cliente/headers.h
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define EOL '\0'
 #define EXIT ":q"
---
+++ server/headers.h
@@ -0,0 +1,17 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define EOL '\0'
+#define EXIT ":q"
+
+#endif
