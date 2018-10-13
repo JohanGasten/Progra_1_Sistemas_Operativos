52820ca5488d1aa50b15e2cbd101226f3fb92287
JohanGasten <gastenx@hotmail.com>
2018-10-12 19:24:22 -0600

    acomodar headers y define

--- client.c
+++ client.c
@@ -1,14 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
-#define EOL '\0'
-#define EXIT ":q"
+#include "headers.h"
 
 int main(){
     //netstat -tupln
